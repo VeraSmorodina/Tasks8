5b3eab9df45969a6c87f4dd8024dbcc79f664443
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-05-01 16:37:42 +0300

    task1, task2

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -3,9 +3,8 @@ package task2;
 public class Main {
     public static int sumDigitsNumber(int number, int result) {
         if (number / 10 > 0) {
-            int remains = number % 10;
+            result += number % 10;
             number = number / 10;
-            result += remains;
             return sumDigitsNumber(number, result);
         }
         return result + number;
