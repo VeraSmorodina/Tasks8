9f6d908bef4e124ffb6332304e9195137f983f98
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-05-02 23:36:52 +0300

    task1, task2

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -2,6 +2,8 @@ package task1;
 
 public class Main {
     public static int exponentiation(int constant, int number, int degree) {
-        return degree == 1 ? number : exponentiation(constant, number * constant, degree - 1);
+        if (degree == 1)
+            return number;
+        return exponentiation(constant, number * constant, degree - 1);
     }
 }
