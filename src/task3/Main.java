a2f324cae0536b411a80da001ff0387597cd4634
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-05-04 21:14:29 +0300

    task3, task4

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
     public static int getLengthList(ArrayList<Integer> array, int result) {
         if (array.isEmpty()) return result;
-        ArrayList<Integer> newList = new ArrayList<>(array.subList(1, array.size()));
-        return getLengthList(newList, result + 1);
+        array.remove(0);
+        return getLengthList(array, result + 1);
     }
 }
