4dd76e4002ce08dca465ed515572415e3bba968d
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-05-07 15:20:00 +0300

    task5, task6

--- src/task5/Main.java
+++ src/task5/Main.java
@@ -3,13 +3,13 @@ package task5;
 import java.util.List;
 
 public class Main {
-    public static void printOdd(List<Integer> numbers) {
-        printOddValues(numbers, 0);
+    public static void printOddValues(List<Integer> numbers) {
+        printOdd(numbers, 0);
     }
 
-    private static void printOddValues(List<Integer> numbers, int index) {
+    private static void printOdd(List<Integer> numbers, int index) {
         if (index >= numbers.size()) return;
         if (numbers.get(index) % 2 == 0) System.out.println(numbers.get(index));
-        printOddValues(numbers, index + 1);
+        printOdd(numbers, index + 1);
     }
 }
--- src/task6/Main.java
+++ src/task6/Main.java
@@ -3,13 +3,13 @@ package task6;
 import java.util.List;
 
 public class Main {
-    public static void printOddIndex(List<Integer> numbers) {
-        printOddIndexs(numbers, 0);
+    public static void printOddIndexes(List<Integer> numbers) {
+        printOddIndex(numbers, 0);
     }
 
-    private static void printOddIndexs(List<Integer> numbers, int index) {
+    private static void printOddIndex(List<Integer> numbers, int index) {
         if (index >= numbers.size()) return;
         System.out.println(numbers.get(index));
-        printOddIndexs(numbers, index + 2);
+        printOddIndex(numbers, index + 2);
     }
 }
