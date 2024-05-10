1ed23466e519d1b7112a7f488a808d754f6119ee
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-05-10 17:54:50 +0300

    task7

--- src/task7/Main.java
+++ src/task7/Main.java
@@ -1,17 +1,14 @@
 package task7;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static int getSecondMaxNumber(List<Integer> numbers) {
         if (numbers.size() == 1) return numbers.get(0);
-        List<Integer> result = new ArrayList<>();
-        if (numbers.get(0) >= numbers.get(1))
-            result = getSecondMaxNumberRec(numbers, 2, numbers.get(0), numbers.get(1));
-        else if (numbers.get(0) < numbers.get(1))
-            result = getSecondMaxNumberRec(numbers, 2, numbers.get(1), numbers.get(0));
-        return result.get(1);
+        boolean b = numbers.get(0) >= numbers.get(1);
+        int max1 = b ? numbers.get(0) : numbers.get(1);
+        int max2 = b ? numbers.get(1) : numbers.get(0);
+        return getSecondMaxNumberRec(numbers, 2, max1, max2).get(1);
     }
 
     private static List<Integer> getSecondMaxNumberRec(List<Integer> numbers, int index, Integer max1, Integer max2) {
