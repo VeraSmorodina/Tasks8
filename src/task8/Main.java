73295e0b13c27bbed6f99b60c5a0111ddffc59a7
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-05-11 19:31:36 +0300

    task8

--- src/task8/Main.java
+++ src/task8/Main.java
@@ -1,33 +1,19 @@
 package task8;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\krainyukov.sv\\IdeaProjects\\Tasks8\\src\\directory");
-
-        List<File> list = met(file);
-        for (File s : list) {
-            System.out.println(s.getName());
-        }
-    }
-
-    public static List met(File file) {
+    public static List<File> getFiles(File file) {
         List<File> dir = new ArrayList<>();
         List<File> listResult = new ArrayList<>();
         if (file.isDirectory())
             dir.add(file);
-        else {
-            System.out.println("Файл не является директорией");
-            return listResult;
-        }
-        return rec(dir, listResult);
+        return getFilesRec(dir, listResult);
     }
 
-    private static List<File> rec(List<File> dir, List<File> listRes) {
+    private static List<File> getFilesRec(List<File> dir, List<File> listRes) {
         if (dir.isEmpty()) return listRes;
         List<File> directories = new ArrayList<>();
         for (File file : dir) {
@@ -40,28 +26,6 @@ public class Main {
                 directories.add(file1);
             }
         }
-        return rec(directories, listRes);
+        return getFilesRec(directories, listRes);
     }
-
-
-//    public static void main(String[] args) throws IOException {
-//        String path = "C:\\Users\\krainyukov.sv\\IdeaProjects\\Tasks8\\src\\directory";
-//        List<String> list = rec(path);
-//        for (String s : list) {
-//            System.out.println(s);
-//        }
-//    }
-//
-//    public static List<String> rec(String dir) throws IOException {
-//        List<String> list = new ArrayList<>();
-//        Stream<Path> files = Files.walk(Path.of(dir));
-//        files.forEach((path) -> {
-//            if (!path.toFile().isDirectory()) {
-//                File my_fil = new File(path.toString());
-//                list.add(my_fil.getName());
-//            }
-//        });
-//        files.close();
-//        return list;
-//    }
 }
---
+++ test/task8/MainTest.java
@@ -0,0 +1,28 @@
+package task8;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static task8.Main.getFiles;
+
+public class MainTest {
+    @Test
+    public void Test1() {
+        File file = new File("C:\\Users\\krainyukov.sv\\IdeaProjects\\Tasks8\\src\\directory");
+        List<File> list = getFiles(file);
+        List<File> res = new ArrayList<>();
+        File file1 = new File("C:\\Users\\krainyukov.sv\\IdeaProjects\\Tasks8\\src\\directory\\file11.txt");
+        File file2 = new File("C:\\Users\\krainyukov.sv\\IdeaProjects\\Tasks8\\src\\directory\\dir11\\file21");
+        File file3 = new File("C:\\Users\\krainyukov.sv\\IdeaProjects\\Tasks8\\src\\directory\\dir11\\file22");
+        File file4 = new File("C:\\Users\\krainyukov.sv\\IdeaProjects\\Tasks8\\src\\directory\\dir11\\dir21\\file31");
+        res.add(file1);
+        res.add(file2);
+        res.add(file3);
+        res.add(file4);
+        assertEquals(res, list);
+    }
+}
